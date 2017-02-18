3203f54f8e70fb0d3a1d3582147e0e9e34c85de7
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-02-18 15:30:28 +0100

    Errors returned as JSON instead of HTML

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.send({
         message: err.message,
         error: err
     });
